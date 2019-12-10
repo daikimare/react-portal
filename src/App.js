49e0378bc24b58203d9b060532ff0b2afd35c187
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2019-12-11 08:54:38 +0900

    初期外装設定

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
-import React,{Component} from 'react';
-import TodoList from './TodoList';
+import React, { Component } from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 import './css/App.css'
 
-class App extends Component{
-  constructor(){
+
+class App extends Component {
+
+  constructor() {
     super()
-    this.state = {
-      todos:[
-        {
-        id:1,
-        title:"Hello,React!",
-        desc:"React始めました",
+    const todos = [
+      {
+        id: 1,
+        title: "Hello",
+        desc: "履修申請",
         done: false
-        },
-        {
-          id:2,
-          title: "Hello,Redux!",
-          desc: "reduxも始めました",
-          done: false
-        },
-      ]
+      },
+      {
+        id: 2,
+        title: "Hello",
+        desc: "教務課",
+        done: false
+      },
+    ]
+    this.state = {
+      todos: todos,
+      countTodo: todos.length + 1,
     }
   }
 
-  render(){
+  handleSubmit(e) {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const desc = e.target.desc.value;
+    const todos = this.state.todos.slice()
+    const countTodo = this.state.countTodo
+
+    todos.push({
+      id: countTodo,
+      title: title,
+      desc: desc,
+      done: false,
+    });
+
+    this.setState({ todos })
+    this.setState({ countTodo: countTodo + 1 })
+
+
+    e.target.title.value = '';
+    e.target.desc.value = '';
+  }
+
+
+  render() {
     return (
-      <div className = "app">
-        <h1>toアプリを作ってみた</h1>
-        <TodoList todos={
-          this.state.todos
-        } />
+      <div className="app">
+        <h1>遷移先</h1>
+        <Form handleSubmit={this.handleSubmit.bind(this)} />
+        <TodoList
+          todos={this.state.todos}
+          />
       </div>
     );
   }
--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';      
 import './css/todo.css';
 
 class Todo extends Component{
-  render(){
-    const className = 'undone'
-    const link = this.props.done ? '元に戻す' : '完了!'
-    return (
-      <li className= {className}>
+  render() {
+    const className = this.props.done ? 'done' : 'undone';
+    const link = this.props.done ? '元に戻す' : '完了！'
+    return(
+      <li className={className}>
         <span>{this.props.id}</span>
-        <span>:{this.props.title}</span>
-        <a href= "">{link}</a>
+        <span>：{this.props.title}　　</span>
+        <button href=　"#" onClick={(e)=>{ e.preventDefault(); this.props.setTodoStatus(this.props)}}>{link}</button>
         <p>{this.props.desc}</p>
       </li>
     );
   }
 }
-
 export default Todo
